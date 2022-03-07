47058a72b758bc647acd7238f7e2b72129cf0555
Jose Manuel Otero Roman <jmoteroroman98@gmail.com>
2022-03-07 18:27:14 +0100

    App.py Linteada

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import locale
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
 
+
 @app.route("/")
 def index():
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
-    
+
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -21,8 +22,10 @@ def index():
     f.write(str(count))
     f.close()
     # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="dogs", count=count)
+    return render_template("index.html", the_time=the_time,
+                           tema="dogs", count=count)
+
 
 if __name__ == "__main__":
     app.run()
-#probando workflows
+# probando workflows
